1f912eefe3e31322fa76652e51c2e1ec5f54bc7e
student <student@MSPU.local>
2019-03-30 12:08:03 +0300

    изменения сделаны

---
+++ company.py
@@ -0,0 +1,7 @@
+company = "google.com"
+if "my" in company:
+    print("Подстрока my найдена")
+elif "google" in company:
+    print("Подстрока google найдена")
+else:
+     print("Подстрока не найдена")
